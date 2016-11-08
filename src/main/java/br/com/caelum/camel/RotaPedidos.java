472f218a2389cbb65fbb60dd32bcca97ff91e2ca
Wellington Moreira <dbaldani@uoldiveo.com>
2016-11-08 09:39:31 -0200

    Exer Cap 1

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -8,6 +9,17 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
-
-	}	
+		context.addRoutes(new RouteBuilder() {
+			
+			@Override
+			public void configure() throws Exception {
+		    	from("file:pedidos").
+		    	  log("${id} - ${body}").
+		    	to("file:saida");
+				
+			}
+		});
+		context.start(); //aqui camel realmente começa a trabalhar
+        Thread.sleep(10000); //esperando um pouco para dar um tempo para camel
+	}
 }
