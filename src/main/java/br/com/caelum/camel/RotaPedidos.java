7bc634e0c9705bba823ebdb6b0890cdea9231b47
Wellington Moreira <dbaldani@uoldiveo.com>
2016-11-09 16:17:05 -0200

    exercicios 2

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -11,10 +12,26 @@ public class RotaPedidos {
 		CamelContext context = new DefaultCamelContext();
 		context.addRoutes(new RouteBuilder() {
 			
+			/* Rota Simples
 			@Override
 			public void configure() throws Exception {
-		    	from("file:pedidos").
+				  from("file:pedidos?delay=5s&noop=true").
+				  	marshal().xmljson().
 		    	  log("${id} - ${body}").
+		    	to("file:saida");
+				
+			}*/
+			// Rota de com filter 
+			@Override
+			public void configure() throws Exception {
+				  from("file:pedidos?delay=5s&noop=true").
+				  	split().
+				  		xpath("/pedido/itens/item").
+				  	filter().
+				  		xpath("/item/formato[text()='EBOOK']").
+				  	log("${id} - ${body}").
+				  	marshal().xmljson().
+				  	setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
 		    	to("file:saida");
 				
 			}
